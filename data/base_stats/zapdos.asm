d45d7f9588dd599c69cc480c4690de0b2b6594e2
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-14 16:49:27 -0500

    Update zapdos.asm

--- data/base_stats/zapdos.asm
+++ data/base_stats/zapdos.asm
@@ -1,16 +1,16 @@
 	db ZAPDOS ; 145
 
-	db 90, 90, 85, 100, 125, 90
+	db  90,  90,  85, 100, 125, 110
 	;   hp  atk  def  spd  sat  sdf
-
+	;                           +20
 	db ELECTRIC, FLYING
 	db 3 ; catch rate
 	db 251 ; base exp
-	db NO_ITEM ; item 1
-	db NO_ITEM ; item 2
+	db BRIGHTPOWDER ; item 1
+	db BRIGHTPOWDER ; item 2
 	db 255 ; gender
 	db 100 ; unknown
-	db 80 ; step cycles to hatch
+	db 16 ; step cycles to hatch
 	db 5 ; unknown
 	dn 7, 7 ; frontpic dimensions
 	db 0, 0, 0, 0 ; padding
