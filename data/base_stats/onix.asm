80704fe77fa21927a775dfd19100f52639a9a65f
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-14 17:53:21 -0500

    Update onix.asm

--- data/base_stats/onix.asm
+++ data/base_stats/onix.asm
@@ -1,16 +1,16 @@
 	db ONIX ; 095
 
-	db 35, 45, 160, 70, 30, 45
+	db  55,  65, 180,  70,  45,  60
 	;   hp  atk  def  spd  sat  sdf
-
+	;  +20  +20  +20       +15  +15
 	db ROCK, GROUND
 	db 45 ; catch rate
 	db 77 ; base exp
-	db NO_ITEM ; item 1
-	db NO_ITEM ; item 2
+	db BITTER_BERRY ; item 1
+	db HARD_STONE ; item 2
 	db 127 ; gender
 	db 100 ; unknown
-	db 25 ; step cycles to hatch
+	db 5 ; step cycles to hatch
 	db 5 ; unknown
 	dn 7, 7 ; frontpic dimensions
 	db 0, 0, 0, 0 ; padding
