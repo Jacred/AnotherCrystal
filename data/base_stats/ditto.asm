a1eb761b5864b1e3801473a1372eb602b249162e
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-14 18:50:41 -0500

    Update ditto.asm

--- data/base_stats/ditto.asm
+++ data/base_stats/ditto.asm
@@ -1,16 +1,16 @@
 	db DITTO ; 132
 
-	db 48, 48, 48, 48, 48, 48
+	db  80,  80,  80,  80,  80,  80
 	;   hp  atk  def  spd  sat  sdf
-
-	db NORMAL, NORMAL
+	;  +32  +32  +32  +32  +32  +32
+	db CURSE_T, CURSE_T
 	db 35 ; catch rate
 	db 101 ; base exp
-	db NO_ITEM ; item 1
-	db NO_ITEM ; item 2
+	db METAL_POWDER ; item 1
+	db METAL_POWDER ; item 2
 	db 255 ; gender
 	db 100 ; unknown
-	db 20 ; step cycles to hatch
+	db 4 ; step cycles to hatch
 	db 5 ; unknown
 	dn 5, 5 ; frontpic dimensions
 	db 0, 0, 0, 0 ; padding
