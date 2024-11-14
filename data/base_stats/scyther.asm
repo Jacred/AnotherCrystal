b57c1b7a1c9c9ff6a244eb27c067e1cd40fcf896
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-14 18:19:49 -0500

    Update scyther.asm

--- data/base_stats/scyther.asm
+++ data/base_stats/scyther.asm
@@ -1,16 +1,16 @@
 	db SCYTHER ; 123
 
-	db 70, 110, 80, 105, 55, 80
+	db  70, 130,  80, 120,  60,  90
 	;   hp  atk  def  spd  sat  sdf
-
+	;       +20       +15   +5  +10
 	db BUG, FLYING
 	db 45 ; catch rate
 	db 100 ; base exp
-	db NO_ITEM ; item 1
-	db NO_ITEM ; item 2
+	db BITTER_BERRY ; item 1
+	db BITTER_BERRY ; item 2
 	db 127 ; gender
 	db 100 ; unknown
-	db 25 ; step cycles to hatch
+	db 5 ; step cycles to hatch
 	db 5 ; unknown
 	dn 7, 7 ; frontpic dimensions
 	db 0, 0, 0, 0 ; padding
