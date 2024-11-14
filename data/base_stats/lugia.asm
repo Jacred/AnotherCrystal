639b601be51bf1955edca51a04c17ce961e43d6b
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-14 16:12:45 -0500

    Update lugia.asm

--- data/base_stats/lugia.asm
+++ data/base_stats/lugia.asm
@@ -1,16 +1,16 @@
 	db LUGIA ; 249
 
-	db 106,  90, 130, 110,  90, 154
+	db 106, 100, 130, 110, 100, 154
 	;   hp  atk  def  spd  sat  sdf
-
+	;       +10            +10
 	db PSYCHIC, FLYING
 	db 3 ; catch rate
 	db 253 ; base exp
-	db NO_ITEM ; item 1
-	db NO_ITEM ; item 2
+	db BRIGHTPOWDER ; item 1
+	db BRIGHTPOWDER ; item 2
 	db 255 ; gender
 	db 100 ; unknown
-	db 120 ; step cycles to hatch
+	db 24 ; step cycles to hatch
 	db 5 ; unknown
 	dn 7, 7 ; frontpic dimensions
 	db 0, 0, 0, 0 ; padding
