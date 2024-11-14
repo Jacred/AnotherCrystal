409efde3394e6b15fa64e94ac21dc14d10e8c8c3
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-14 12:47:33 -0500

    Update venusaur.asm

--- data/base_stats/venusaur.asm
+++ data/base_stats/venusaur.asm
@@ -1,16 +1,16 @@
 	db VENUSAUR ; 003
 
-	db 80, 82, 83, 80, 100, 100
+	db  80,  92, 103,  80, 110, 110
 	;   hp  atk  def  spd  sat  sdf
-
+	;       +10  +20       +10  +10
 	db GRASS, POISON
-	db 45 ; catch rate
+	db 9 ; catch rate
 	db 236 ; base exp
-	db NO_ITEM ; item 1
-	db NO_ITEM ; item 2
+	db LEAF_STONE ; item 1
+	db LEAF_STONE ; item 2
 	db 31 ; gender
 	db 100 ; unknown
-	db 20 ; step cycles to hatch
+	db 4 ; step cycles to hatch
 	db 5 ; unknown
 	dn 7, 7 ; frontpic dimensions
 	db 0, 0, 0, 0 ; padding
@@ -18,5 +18,5 @@
 	dn MONSTER, PLANT ; egg groups
 
 	; tmhm
-	tmhm CURSE, BODY_SLAM, TOXIC, ROCK_SMASH, HIDDEN_POWER, SUNNY_DAY, EARTH_POWER, HYPER_BEAM, PROTECT, GIGA_DRAIN, SOLARBEAM, EARTHQUAKE, RETURN, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SLUDGE_BOMB, SWORDS_DANCE, STRING_SHOT, REST, ATTRACT, FURY_CUTTER, SUBSTITUTE, CUT, STRENGTH, FLASH, FALSE_SWIPE
+	tmhm CURSE, BODY_SLAM, TOXIC, ROCK_SMASH, HIDDEN_POWER, SUNNY_DAY, EARTH_POWER, HYPER_BEAM, PROTECT, GIGA_DRAIN, SOLARBEAM, EARTHQUAKE, RETURN, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SLUDGE_BOMB, SWORDS_DANCE, REST, ATTRACT, FURY_CUTTER, SUBSTITUTE, CUT, STRENGTH, FLASH, FALSE_SWIPE
 	; end
