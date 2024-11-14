f6727297d60416ef2433c8d137c633d32eac310d
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-14 18:42:22 -0500

    Update murkrow.asm

--- data/base_stats/murkrow.asm
+++ data/base_stats/murkrow.asm
@@ -1,17 +1,16 @@
 	db MURKROW ; 198
 
-	db  85,  105, 52,  91, 105,  52 ; BST 405 > 490
+	db  80, 105,  47,  81,  95,  47
 	;   hp  atk  def  spd  sat  sdf
-;	db  60,  85,  42,  91,  85,  42
-
+	;  +20  +20   +5  -10  +10   +5
 	db DARK, FLYING
 	db 30 ; catch rate
 	db 81 ; base exp
-	db NO_ITEM ; item 1
-	db NO_ITEM ; item 2
+	db MINT_BERRY ; item 1
+	db MINT_BERRY ; item 2
 	db 127 ; gender
 	db 100 ; unknown
-	db 20 ; step cycles to hatch
+	db 4 ; step cycles to hatch
 	db 5 ; unknown
 	dn 6, 6 ; frontpic dimensions
 	db 0, 0, 0, 0 ; padding
