59c0aaffb0339b64947a497be68c258ed3a12f3a
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-14 16:48:01 -0500

    Update larvitar.asm

--- data/base_stats/larvitar.asm
+++ data/base_stats/larvitar.asm
@@ -1,16 +1,16 @@
 	db LARVITAR ; 246
 
-	db  50,  64,  50,  41,  45,  50
+	db  50,  79,  70,  46,  45,  60
 	;   hp  atk  def  spd  sat  sdf
-
+	;       +15  +20   +5       +10
 	db ROCK, GROUND
 	db 45 ; catch rate
 	db 60 ; base exp
-	db NO_ITEM ; item 1
-	db NO_ITEM ; item 2
+	db BERRY ; item 1
+	db HARD_STONE ; item 2
 	db 127 ; gender
 	db 100 ; unknown
-	db 40 ; step cycles to hatch
+	db 8 ; step cycles to hatch
 	db 5 ; unknown
 	dn 5, 5 ; frontpic dimensions
 	db 0, 0, 0, 0 ; padding
