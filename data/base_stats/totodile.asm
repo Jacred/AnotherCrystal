f85e796e3cf03159d02595578bcf2ac663078759
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-14 14:30:59 -0500

    Update totodile.asm

--- data/base_stats/totodile.asm
+++ data/base_stats/totodile.asm
@@ -1,16 +1,16 @@
 	db TOTODILE ; 158
 
-	db  50,  65,  64,  43,  44,  48
+	db  50,  65,  65,  43,  49,  48
 	;   hp  atk  def  spd  sat  sdf
-
+	;             +1        +5
 	db WATER, WATER
 	db 45 ; catch rate
 	db 63 ; base exp
-	db NO_ITEM ; item 1
-	db NO_ITEM ; item 2
+	db BERRY ; item 1
+	db MYSTIC_WATER ; item 2
 	db 31 ; gender
 	db 100 ; unknown
-	db 20 ; step cycles to hatch
+	db 5 ; step cycles to hatch
 	db 5 ; unknown
 	dn 5, 5 ; frontpic dimensions
 	db 0, 0, 0, 0 ; padding
