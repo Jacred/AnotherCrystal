93a1025134a6de2c6643ea6a0492b16bea4507fc
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-14 12:48:11 -0500

    Update ivysaur.asm

--- data/base_stats/ivysaur.asm
+++ data/base_stats/ivysaur.asm
@@ -1,16 +1,16 @@
 	db IVYSAUR ; 002
 
-	db 60, 62, 63, 60, 80, 80
+	db  60,  67,  73,  60,  85,  85
 	;   hp  atk  def  spd  sat  sdf
-
+	;        +5  +10        +5   +5
 	db GRASS, POISON
-	db 45 ; catch rate
+	db 27 ; catch rate
 	db 142 ; base exp
-	db NO_ITEM ; item 1
-	db NO_ITEM ; item 2
+	db BITTER_BERRY ; item 1
+	db MIRACLE_SEED ; item 2
 	db 31 ; gender
 	db 100 ; unknown
-	db 20 ; step cycles to hatch
+	db 4 ; step cycles to hatch
 	db 5 ; unknown
 	dn 6, 6 ; frontpic dimensions
 	db 0, 0, 0, 0 ; padding
@@ -18,5 +18,5 @@
 	dn MONSTER, PLANT ; egg groups
 
 	; tmhm
-	tmhm CURSE, BODY_SLAM, TOXIC, ROCK_SMASH, HIDDEN_POWER, SUNNY_DAY, PROTECT, GIGA_DRAIN, SOLARBEAM, RETURN, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SLUDGE_BOMB, SWORDS_DANCE, STRING_SHOT, REST, ATTRACT, FURY_CUTTER, SUBSTITUTE, CUT,STRENGTH, FLASH, FALSE_SWIPE
+	tmhm CURSE, BODY_SLAM, TOXIC, ROCK_SMASH, HIDDEN_POWER, SUNNY_DAY, PROTECT, GIGA_DRAIN, SOLARBEAM, RETURN, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SLUDGE_BOMB, SWORDS_DANCE, REST, ATTRACT, FURY_CUTTER, SUBSTITUTE, CUT,STRENGTH, FLASH, FALSE_SWIPE
 	; end
