858cabb05c7201987520c94f4c2a5dd2b26f0f3f
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-14 14:07:27 -0500

    Update bulbasaur.asm

--- data/base_stats/bulbasaur.asm
+++ data/base_stats/bulbasaur.asm
@@ -1,16 +1,16 @@
 	db BULBASAUR ; 001
 
-	db 45, 49, 49, 45, 65, 65
+	db  45,  52,  53,  45,  65,  65
 	;   hp  atk  def  spd  sat  sdf
-
+	;        +3   +4
 	db GRASS, POISON
 	db 45 ; catch rate
 	db 64 ; base exp
-	db NO_ITEM ; item 1
-	db NO_ITEM ; item 2
+	db BITTER_BERRY ; item 1
+	db MIRACLE_SEED ; item 2
 	db 31 ; gender
 	db 100 ; unknown
-	db 20 ; step cycles to hatch
+	db 4 ; step cycles to hatch
 	db 5 ; unknown
 	dn 5, 5 ; frontpic dimensions
 	db 0, 0, 0, 0 ; padding
@@ -18,6 +18,6 @@
 	dn MONSTER, PLANT ; egg groups
 
 	; tmhm
-	tmhm CURSE, BODY_SLAM, TOXIC, ROCK_SMASH, HIDDEN_POWER, SUNNY_DAY, PROTECT, GIGA_DRAIN, SOLARBEAM, RETURN, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SLUDGE_BOMB, SWORDS_DANCE, STRING_SHOT, REST, ATTRACT, FURY_CUTTER, SUBSTITUTE, CUT,STRENGTH, FLASH, FALSE_SWIPE
+	tmhm CURSE, BODY_SLAM, TOXIC, ROCK_SMASH, HIDDEN_POWER, SUNNY_DAY, PROTECT, GIGA_DRAIN, SOLARBEAM, RETURN, DOUBLE_TEAM, SWAGGER, SLEEP_TALK, SLUDGE_BOMB, SWORDS_DANCE, REST, ATTRACT, FURY_CUTTER, SUBSTITUTE, CUT,STRENGTH, FLASH, FALSE_SWIPE
 	; end
 
