149de92ed3db9f563848a14c5700a787c52c2d4a
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-14 14:30:56 -0500

    Update croconaw.asm

--- data/base_stats/croconaw.asm
+++ data/base_stats/croconaw.asm
@@ -1,16 +1,16 @@
 	db CROCONAW ; 159
 
-	db  65,  80,  80,  58,  59,  63
+	db  65,  85,  85,  58,  69,  68
 	;   hp  atk  def  spd  sat  sdf
-
+	;        +5   +5       +10   +5
 	db WATER, WATER
-	db 45 ; catch rate
+	db 27 ; catch rate
 	db 142 ; base exp
-	db NO_ITEM ; item 1
-	db NO_ITEM ; item 2
+	db GOLD_BERRY ; item 1
+	db MYSTIC_WATER ; item 2
 	db 31 ; gender
 	db 100 ; unknown
-	db 20 ; step cycles to hatch
+	db 5 ; step cycles to hatch
 	db 5 ; unknown
 	dn 6, 6 ; frontpic dimensions
 	db 0, 0, 0, 0 ; padding
