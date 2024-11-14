57e187d9daa5c1a7e417e6e69bffe2518fcf0c2d
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-14 16:49:02 -0500

    Update articuno.asm

--- data/base_stats/articuno.asm
+++ data/base_stats/articuno.asm
@@ -1,16 +1,16 @@
 	db ARTICUNO ; 144
 
-	db 90, 85, 100, 85,  95, 125
+	db  90,  85, 100,  85, 115, 125
 	;   hp  atk  def  spd  sat  sdf
-
+	;                      +20
 	db ICE, FLYING
 	db 3 ; catch rate
 	db 251 ; base exp
-	db NO_ITEM ; item 1
-	db NO_ITEM ; item 2
+	db BRIGHTPOWDER ; item 1
+	db BRIGHTPOWDER ; item 2
 	db 255 ; gender
 	db 100 ; unknown
-	db 80 ; step cycles to hatch
+	db 16 ; step cycles to hatch
 	db 5 ; unknown
 	dn 7, 7 ; frontpic dimensions
 	db 0, 0, 0, 0 ; padding
