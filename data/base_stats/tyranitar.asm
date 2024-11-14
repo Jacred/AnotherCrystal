350747d4f50d0c4a023332958bf4a6331fedcf61
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-14 16:46:14 -0500

    Update tyranitar.asm

--- data/base_stats/tyranitar.asm
+++ data/base_stats/tyranitar.asm
@@ -4,13 +4,13 @@
 	;   hp  atk  def  spd  sat  sdf
 
 	db ROCK, DARK
-	db 45 ; catch rate
+	db 9 ; catch rate
 	db 252 ; base exp
-	db NO_ITEM ; item 1
-	db NO_ITEM ; item 2
+	db NUGGET ; item 1
+	db NUGGET ; item 2
 	db 127 ; gender
 	db 100 ; unknown
-	db 40 ; step cycles to hatch
+	db 8 ; step cycles to hatch
 	db 5 ; unknown
 	dn 7, 7 ; frontpic dimensions
 	db 0, 0, 0, 0 ; padding
