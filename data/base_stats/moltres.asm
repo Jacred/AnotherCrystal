cbc0eae1349d66da3c02d48d62fb02b0f752c892
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-14 16:49:57 -0500

    Update moltres.asm

--- data/base_stats/moltres.asm
+++ data/base_stats/moltres.asm
@@ -1,16 +1,16 @@
 	db MOLTRES ; 146
 
-	db 90, 100, 90, 90, 125, 85
+	db  90, 100,  90,  90, 125, 105
 	;   hp  atk  def  spd  sat  sdf
-
+	;                           +20
 	db FIRE, FLYING
 	db 3 ; catch rate
 	db 251 ; base exp
-	db NO_ITEM ; item 1
-	db NO_ITEM ; item 2
+	db BRIGHTPOWDER ; item 1
+	db BRIGHTPOWDER ; item 2
 	db 255 ; gender
 	db 100 ; unknown
-	db 80 ; step cycles to hatch
+	db 16 ; step cycles to hatch
 	db 5 ; unknown
 	dn 7, 7 ; frontpic dimensions
 	db 0, 0, 0, 0 ; padding
