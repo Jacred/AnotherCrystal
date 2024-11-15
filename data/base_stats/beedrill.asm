d801f2cac7254e7193d8786fe990e2e9323a349f
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-14 19:42:51 -0500

    Update beedrill.asm

--- data/base_stats/beedrill.asm
+++ data/base_stats/beedrill.asm
@@ -1,16 +1,16 @@
 	db BEEDRILL ; 015
 
-	db 105, 100, 80, 85, 60, 95
+	db  65, 150,  40, 145,  15,  80
 	;   hp  atk  def  spd  sat  sdf
-
+	;       +70       +70  -30
 	db BUG, POISON
 	db 45 ; catch rate
 	db 173 ; base exp
-	db NO_ITEM ; item 1
+	db POISON_BARB ; item 1
 	db POISON_BARB ; item 2
 	db 127 ; gender
 	db 100 ; unknown
-	db 15 ; step cycles to hatch
+	db 3 ; step cycles to hatch
 	db 5 ; unknown
 	dn 7, 7 ; frontpic dimensions
 	db 0, 0, 0, 0 ; padding
