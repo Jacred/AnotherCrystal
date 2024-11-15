44322bf2206b846664056d941a4f668db1322048
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-14 23:21:00 -0500

    Update jynx.asm

--- data/base_stats/jynx.asm
+++ data/base_stats/jynx.asm
@@ -1,8 +1,8 @@
 	db JYNX ; 124
 
-	db 65, 50, 35, 95, 115, 95
+	db  75,  83,  67,  95, 125,  95
 	;   hp  atk  def  spd  sat  sdf
-
+	;  +10  +33  +32       +10
 	db ICE, PSYCHIC
 	db 45 ; catch rate
 	db 159 ; base exp
@@ -10,7 +10,7 @@
 	db ICE_BERRY ; item 2
 	db 254 ; gender
 	db 100 ; unknown
-	db 25 ; step cycles to hatch
+	db 5 ; step cycles to hatch
 	db 5 ; unknown
 	dn 6, 6 ; frontpic dimensions
 	db 0, 0, 0, 0 ; padding
