297c9e104ada9e5185d28b9a0fdda303697971b4
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-14 22:32:57 -0500

    Update sandshrew.asm

--- data/base_stats/sandshrew.asm
+++ data/base_stats/sandshrew.asm
@@ -1,16 +1,16 @@
 	db SANDSHREW ; 027
 
-	db 50, 75, 85, 40, 20, 30
+	db  50,  75,  85,  40,  20,  40
 	;   hp  atk  def  spd  sat  sdf
-
+	;                      +10  +10
 	db GROUND, GROUND
 	db 255 ; catch rate
 	db 60 ; base exp
 	db NO_ITEM ; item 1
-	db NO_ITEM ; item 2
+	db BERRY ; item 2
 	db 127 ; gender
 	db 100 ; unknown
-	db 20 ; step cycles to hatch
+	db 4 ; step cycles to hatch
 	db 5 ; unknown
 	dn 5, 5 ; frontpic dimensions
 	db 0, 0, 0, 0 ; padding
