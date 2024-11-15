6f61e91ea9174aa7286698dabcf433cb243959f8
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-14 20:51:38 -0500

    Update rattata.asm

--- data/base_stats/rattata.asm
+++ data/base_stats/rattata.asm
@@ -1,16 +1,16 @@
 	db RATTATA ; 019
 
-	db 30, 56, 35, 72, 25, 35
+	db  40,  61,  40,  72,  30,  40
 	;   hp  atk  def  spd  sat  sdf
-
+	;  +10   +5   +5        +5   +5
 	db NORMAL, NORMAL
 	db 255 ; catch rate
 	db 51 ; base exp
 	db NO_ITEM ; item 1
-	db NO_ITEM ; item 2
+	db BERRY ; item 2
 	db 127 ; gender
 	db 100 ; unknown
-	db 15 ; step cycles to hatch
+	db 3 ; step cycles to hatch
 	db 5 ; unknown
 	dn 5, 5 ; frontpic dimensions
 	db 0, 0, 0, 0 ; padding
