c191e879e8297c62609e50c363368ac6ffccd416
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-14 21:41:50 -0500

    Update spearow.asm

--- data/base_stats/spearow.asm
+++ data/base_stats/spearow.asm
@@ -1,13 +1,13 @@
 	db SPEAROW ; 021
 
-	db 40, 60, 30, 70, 31, 31
+	db  40,  70,  40,  70,  35,  35
 	;   hp  atk  def  spd  sat  sdf
-
+	;       +10  +10        +4   +4
 	db NORMAL, FLYING
 	db 255 ; catch rate
 	db 52 ; base exp
 	db NO_ITEM ; item 1
-	db NO_ITEM ; item 2
+	db BERRY ; item 2
 	db 127 ; gender
 	db 100 ; unknown
 	db 15 ; step cycles to hatch
