5650a92568ba58ff1f48fe6b04a6a489e48548bb
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-14 22:33:09 -0500

    Update sandslash.asm

--- data/base_stats/sandslash.asm
+++ data/base_stats/sandslash.asm
@@ -1,16 +1,16 @@
 	db SANDSLASH ; 028
 
-	db 75, 100, 110, 65, 45, 55
+	db  75, 100, 120,  75,  55,  75
 	;   hp  atk  def  spd  sat  sdf
-
-	db GROUND, GROUND
+	;                 +10  +10  +20
+	db GROUND, STEEL
 	db 90 ; catch rate
 	db 158 ; base exp
-	db NO_ITEM ; item 1
-	db NO_ITEM ; item 2
+	db BERRY ; item 1
+	db SOFT_SAND ; item 2
 	db 127 ; gender
 	db 100 ; unknown
-	db 20 ; step cycles to hatch
+	db 4 ; step cycles to hatch
 	db 5 ; unknown
 	dn 6, 6 ; frontpic dimensions
 	db 0, 0, 0, 0 ; padding
