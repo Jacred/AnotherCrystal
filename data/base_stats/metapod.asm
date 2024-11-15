9ec581d42ad9b0265bcdb7fba680a0afc6f91717
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-14 19:38:21 -0500

    Update metapod.asm

--- data/base_stats/metapod.asm
+++ data/base_stats/metapod.asm
@@ -1,16 +1,16 @@
 	db METAPOD ; 011
 
-	db 50, 20, 55, 30, 25, 25
+	db  60,  20,  80,  20,  30,  40
 	;   hp  atk  def  spd  sat  sdf
-
+	;  +10       +25  -10   +5  +15
 	db BUG, BUG
 	db 120 ; catch rate
 	db 72 ; base exp
-	db NO_ITEM ; item 1
-	db NO_ITEM ; item 2
+	db BERRY ; item 1
+	db SILVERPOWDER ; item 2
 	db 127 ; gender
 	db 100 ; unknown
-	db 15 ; step cycles to hatch
+	db 3 ; step cycles to hatch
 	db 5 ; unknown
 	dn 5, 5 ; frontpic dimensions
 	db 0, 0, 0, 0 ; padding
