767ca1631a564a935b375742483786c5660c47de
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-14 20:47:08 -0500

    Update pidgeot.asm

--- data/base_stats/pidgeot.asm
+++ data/base_stats/pidgeot.asm
@@ -1,16 +1,16 @@
 	db PIDGEOT ; 018
 
-	db 84,  90,  95,  101,  80, 80
+	db  93, 100,  80, 121, 100,  80
 	;   hp  atk  def  spd  sat  sdf
-
+	;  +10  +20   +5  +30  +30  +10
 	db NORMAL, FLYING
 	db 45 ; catch rate
 	db 211 ; base exp
-	db NO_ITEM ; item 1
-	db NO_ITEM ; item 2
+	db SHARP_BEAK ; item 1
+	db SHARP_BEAK ; item 2
 	db 127 ; gender
 	db 100 ; unknown
-	db 15 ; step cycles to hatch
+	db 3 ; step cycles to hatch
 	db 5 ; unknown
 	dn 7, 7 ; frontpic dimensions
 	db 0, 0, 0, 0 ; padding
