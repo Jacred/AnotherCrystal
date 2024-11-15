758e913eb12e284608e3348ac8bdf2571bc87b76
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-15 17:34:13 -0500

    Update kingdra.asm

--- data/base_stats/kingdra.asm
+++ data/base_stats/kingdra.asm
@@ -1,16 +1,16 @@
 	db KINGDRA ; 230
 
-	db  75,  95,  95,  85,  95,  95
+	db  85, 110,  95,  85, 110,  95
 	;   hp  atk  def  spd  sat  sdf
-
+	;  +10  +15            +25
 	db WATER, DRAGON
 	db 45 ; catch rate
 	db 243 ; base exp
-	db NO_ITEM ; item 1
+	db DRAGON_SCALE ; item 1
 	db DRAGON_SCALE ; item 2
 	db 127 ; gender
 	db 100 ; unknown
-	db 20 ; step cycles to hatch
+	db 4 ; step cycles to hatch
 	db 5 ; unknown
 	dn 7, 7 ; frontpic dimensions
 	db 0, 0, 0, 0 ; padding
