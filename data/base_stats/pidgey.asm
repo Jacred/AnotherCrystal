19adbcfea9857158f32b7fd1934ac234f947255a
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-14 20:47:18 -0500

    Update pidgey.asm

--- data/base_stats/pidgey.asm
+++ data/base_stats/pidgey.asm
@@ -1,16 +1,16 @@
 	db PIDGEY ; 016
 
-	db 40, 45, 40, 56, 35, 35
+	db  43,  50,  40,  61,  50,  40
 	;   hp  atk  def  spd  sat  sdf
-
+	;   +3   +5        +5  +15   +5
 	db NORMAL, FLYING
 	db 255 ; catch rate
 	db 50 ; base exp
 	db NO_ITEM ; item 1
-	db NO_ITEM ; item 2
+	db BERRY ; item 2
 	db 127 ; gender
 	db 100 ; unknown
-	db 15 ; step cycles to hatch
+	db 3 ; step cycles to hatch
 	db 5 ; unknown
 	dn 5, 5 ; frontpic dimensions
 	db 0, 0, 0, 0 ; padding
