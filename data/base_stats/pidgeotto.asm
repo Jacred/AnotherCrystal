4e9b1089d7174253e5a77b4a2077a22c3a914c41
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-14 20:47:25 -0500

    Update pidgeotto.asm

--- data/base_stats/pidgeotto.asm
+++ data/base_stats/pidgeotto.asm
@@ -1,16 +1,16 @@
 	db PIDGEOTTO ; 017
 
-	db 63, 60, 55, 71, 50, 50
+	db  63,  70,  55,  86,  70,  55
 	;   hp  atk  def  spd  sat  sdf
-
+	;       +10       +15  +20   +5
 	db NORMAL, FLYING
 	db 120 ; catch rate
 	db 122 ; base exp
-	db NO_ITEM ; item 1
-	db NO_ITEM ; item 2
+	db BERRY ; item 1
+	db SHARP_BEAK ; item 2
 	db 127 ; gender
 	db 100 ; unknown
-	db 15 ; step cycles to hatch
+	db 3 ; step cycles to hatch
 	db 5 ; unknown
 	dn 6, 6 ; frontpic dimensions
 	db 0, 0, 0, 0 ; padding
