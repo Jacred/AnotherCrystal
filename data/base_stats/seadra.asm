3f259c067151e23e1a15af24b890e5046bea815c
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-15 17:34:10 -0500

    Update seadra.asm

--- data/base_stats/seadra.asm
+++ data/base_stats/seadra.asm
@@ -1,16 +1,16 @@
 	db SEADRA ; 117
 
-	db 55, 65, 95, 85, 95, 45
+	db  65,  80,  95,  85,  95,  70
 	;   hp  atk  def  spd  sat  sdf
-
+	;  +10  +15                 +25
 	db WATER, WATER
 	db 75 ; catch rate
 	db 154 ; base exp
-	db NO_ITEM ; item 1
+	db GOLD_LEAF ; item 1
 	db DRAGON_SCALE ; item 2
 	db 127 ; gender
 	db 100 ; unknown
-	db 20 ; step cycles to hatch
+	db 4 ; step cycles to hatch
 	db 5 ; unknown
 	dn 6, 6 ; frontpic dimensions
 	db 0, 0, 0, 0 ; padding
