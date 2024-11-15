e20773cf95a5d5e68dbab2edce360218e7dfe161
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-15 14:15:01 -0500

    Update stantler.asm

--- data/base_stats/stantler.asm
+++ data/base_stats/stantler.asm
@@ -1,17 +1,17 @@
 	db STANTLER ; 234
 
-	db  88,  95,  62,  85,  85,  65 ; BST 465 > 480
+	db  88, 105,  72,  75, 105,  75 ; BST 465 > 520
 	;   hp  atk  def  spd  sat  sdf
-;	db  73,  95,  62,  85,  85,  65 old stats
+	;  +15  +10  +10  -10  +20  +10
 
 	db NORMAL, NORMAL
 	db 45 ; catch rate
 	db 163 ; base exp
-	db NO_ITEM ; item 1
-	db NO_ITEM ; item 2
+	db BITTER_BERRY ; item 1
+	db BITTER_BERRY ; item 2
 	db 127 ; gender
 	db 100 ; unknown
-	db 20 ; step cycles to hatch
+	db 4 ; step cycles to hatch
 	db 5 ; unknown
 	dn 7, 7 ; frontpic dimensions
 	db 0, 0, 0, 0 ; padding
