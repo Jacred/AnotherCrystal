6898394200af6349eae35954389d6c5b4668d6d4
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-15 14:18:04 -0500

    Update porygon.asm

--- data/base_stats/porygon.asm
+++ data/base_stats/porygon.asm
@@ -1,16 +1,16 @@
 	db PORYGON ; 137
 
-	db 65, 60, 70, 40, 85, 75
+	db  75,  70,  80,  50,  95,  85
 	;   hp  atk  def  spd  sat  sdf
-
+	;  +10  +10  +10  +10  +10  +10
 	db NORMAL, NORMAL
 	db 45 ; catch rate
 	db 79 ; base exp
-	db NO_ITEM ; item 1
-	db NO_ITEM ; item 2
+	db BITTER_BERRY ; item 1
+	db BITTER_BERRY ; item 2
 	db 255 ; gender
 	db 100 ; unknown
-	db 20 ; step cycles to hatch
+	db 4 ; step cycles to hatch
 	db 5 ; unknown
 	dn 6, 6 ; frontpic dimensions
 	db 0, 0, 0, 0 ; padding
