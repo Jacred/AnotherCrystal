ab8129dd3ffa13e92549539b81d38c6d901d7d68
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-15 17:38:05 -0500

    Update heracross.asm

--- data/base_stats/heracross.asm
+++ data/base_stats/heracross.asm
@@ -1,16 +1,16 @@
 	db HERACROSS ; 214
 
-	db  80, 125,  75,  85,  40,  95
+	db  80, 155,  95,  80,  40, 100
 	;   hp  atk  def  spd  sat  sdf
-
+	;       +30  +20   -5        +5
 	db BUG, FIGHTING
 	db 45 ; catch rate
 	db 175 ; base exp
-	db NO_ITEM ; item 1
-	db NO_ITEM ; item 2
+	db BITTER_BERRY ; item 1
+	db LEFTOVERS ; item 2
 	db 127 ; gender
 	db 100 ; unknown
-	db 25 ; step cycles to hatch
+	db 5 ; step cycles to hatch
 	db 5 ; unknown
 	dn 6, 6 ; frontpic dimensions
 	db 0, 0, 0, 0 ; padding
