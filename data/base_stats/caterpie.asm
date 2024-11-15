c54a4bdf2bd0501ffe38d0f2616c240e02376958
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-14 19:38:16 -0500

    Update caterpie.asm

--- data/base_stats/caterpie.asm
+++ data/base_stats/caterpie.asm
@@ -1,16 +1,16 @@
 	db CATERPIE ; 010
 
-	db 45, 30, 35, 45, 20, 20
+	db  50,  40,  40,  50,  20,  30
 	;   hp  atk  def  spd  sat  sdf
-
+	;   +5  +10   +5   +5       +10
 	db BUG, BUG
 	db 255 ; catch rate
 	db 39 ; base exp
 	db NO_ITEM ; item 1
-	db NO_ITEM ; item 2
+	db BERRY ; item 2
 	db 127 ; gender
 	db 100 ; unknown
-	db 15 ; step cycles to hatch
+	db 3 ; step cycles to hatch
 	db 5 ; unknown
 	dn 5, 5 ; frontpic dimensions
 	db 0, 0, 0, 0 ; padding
