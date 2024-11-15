163c7599d5b14ad7fbd64d3ba2fbb037dc75c628
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-14 19:42:39 -0500

    Update kakuna.asm

--- data/base_stats/kakuna.asm
+++ data/base_stats/kakuna.asm
@@ -1,16 +1,16 @@
 	db KAKUNA ; 014
 
-	db 45, 25, 50, 35, 25, 25
+	db  50,  30,  70,  30,  30,  40
 	;   hp  atk  def  spd  sat  sdf
-
+	;   +5   +5  +20   -5   +5  +15
 	db BUG, POISON
 	db 120 ; catch rate
 	db 72 ; base exp
-	db NO_ITEM ; item 1
-	db NO_ITEM ; item 2
+	db BERRY ; item 1
+	db POISON_BARB ; item 2
 	db 127 ; gender
 	db 100 ; unknown
-	db 15 ; step cycles to hatch
+	db 3 ; step cycles to hatch
 	db 5 ; unknown
 	dn 5, 5 ; frontpic dimensions
 	db 0, 0, 0, 0 ; padding
