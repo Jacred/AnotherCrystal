da0ade2f48cbff0c386880250940f452be89cef4
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-16 12:58:56 -0500

    Update arcanine.asm

--- data/base_stats/arcanine.asm
+++ data/base_stats/arcanine.asm
@@ -1,16 +1,16 @@
 	db ARCANINE ; 059
 
-	db 90, 110, 80, 100, 100, 80
+	db  90, 110,  90, 110, 110,  90
 	;   hp  atk  def  spd  sat  sdf
-
+	;            +10  +15  +10  +10
 	db FIRE, FIRE
 	db 75 ; catch rate
 	db 194 ; base exp
-	db BURNT_BERRY ; item 1
-	db BURNT_BERRY ; item 2
+	db GOLD_LEAF ; item 1
+	db FIRE_STONE ; item 2
 	db 63 ; gender
 	db 100 ; unknown
-	db 20 ; step cycles to hatch
+	db 4 ; step cycles to hatch
 	db 5 ; unknown
 	dn 7, 7 ; frontpic dimensions
 	db 0, 0, 0, 0 ; padding
