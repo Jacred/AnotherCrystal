43e48ba54e8a289209770ee14c03da9c49f76304
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-16 13:01:08 -0500

    Update ariados.asm

--- data/base_stats/ariados.asm
+++ data/base_stats/ariados.asm
@@ -1,16 +1,16 @@
 	db ARIADOS ; 168
 
-	db  91, 117,  91,  52,  78,  78
+	db  90, 115,  90,  50,  80,  80
 	;   hp  atk  def  spd  sat  sdf
-
+	;  +20  +25  +20  +10  +20  +20
 	db BUG, POISON
 	db 90 ; catch rate
 	db 137 ; base exp
-	db NO_ITEM ; item 1
-	db NO_ITEM ; item 2
+	db BERRY ; item 1
+	db GOLD_BERRY ; item 2
 	db 127 ; gender
 	db 100 ; unknown
-	db 15 ; step cycles to hatch
+	db 3 ; step cycles to hatch
 	db 5 ; unknown
 	dn 7, 7 ; frontpic dimensions
 	db 0, 0, 0, 0 ; padding
