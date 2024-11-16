8fa148fa127ecd3629a287c81eba0f3981c7f0ea
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-16 13:05:11 -0500

    Update marill.asm

--- data/base_stats/marill.asm
+++ data/base_stats/marill.asm
@@ -1,17 +1,16 @@
 	db MARILL ; 183
 
-	db  70,  50,  50,  40,  40,  50 ; BST 250 > 300
+	db 100,  45,  60,  50,  45,  60
 	;   hp  atk  def  spd  sat  sdf
-;	db  70,  20,  50,  40,  20,  50 old stats
-
+	;  +30  +25  +10  +10  +25  +10
 	db WATER, FAIRY_T
 	db 190 ; catch rate
 	db 88 ; base exp
-	db NO_ITEM ; item 1
-	db NO_ITEM ; item 2
+	db BERRY ; item 1
+	db RARE_CANDY ; item 2
 	db 127 ; gender
 	db 100 ; unknown
-	db 20 ; step cycles to hatch
+	db 4 ; step cycles to hatch
 	db 5 ; unknown
 	dn 5, 5 ; frontpic dimensions
 	db 0, 0, 0, 0 ; padding
