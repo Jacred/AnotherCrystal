af35772274aa0748d81192386ce4940320b39a60
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-16 13:04:27 -0500

    Update azumarill.asm

--- data/base_stats/azumarill.asm
+++ data/base_stats/azumarill.asm
@@ -1,16 +1,16 @@
 	db AZUMARILL ; 184
 
-	db 100, 110,  80,  50,  60,  80
+	db 130,  75,  90,  60,  85,  90
 	;   hp  atk  def  spd  sat  sdf
-
+	;  +30  +25  +10  +10  +35  +10
 	db WATER, FAIRY_T
 	db 75 ; catch rate
 	db 185 ; base exp
-	db NO_ITEM ; item 1
-	db NO_ITEM ; item 2
+	db GOLD_LEAF ; item 1
+	db RARE_CANDY ; item 2
 	db 127 ; gender
 	db 100 ; unknown
-	db 20 ; step cycles to hatch
+	db 4 ; step cycles to hatch
 	db 5 ; unknown
 	dn 7, 7 ; frontpic dimensions
 	db 0, 0, 0, 0 ; padding
