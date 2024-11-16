335e4edd9a93980718dd6429f49b76cb522463db
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-16 12:56:05 -0500

    Update aipom.asm

--- data/base_stats/aipom.asm
+++ data/base_stats/aipom.asm
@@ -2,15 +2,15 @@
 
 	db  75, 100,  66, 115,  60,  66
 	;   hp  atk  def  spd  sat  sdf
-
+	;  +20  +30  +11  +30  +20  +11
 	db NORMAL, NORMAL
 	db 45 ; catch rate
 	db 72 ; base exp
-	db NO_ITEM ; item 1
-	db NO_ITEM ; item 2
+	db MYSTERYBERRY ; item 1
+	db MYSTERYBERRY ; item 2
 	db 127 ; gender
 	db 100 ; unknown
-	db 20 ; step cycles to hatch
+	db 4 ; step cycles to hatch
 	db 5 ; unknown
 	dn 5, 5 ; frontpic dimensions
 	db 0, 0, 0, 0 ; padding
