b42512b98b5a34f7f29a2c5298a9e21efcc558b4
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-16 12:25:44 -0500

    Update kadabra.asm

--- data/base_stats/kadabra.asm
+++ data/base_stats/kadabra.asm
@@ -1,16 +1,16 @@
 	db KADABRA ; 064
 
-	db 40, 35, 30, 105, 120, 70
+	db  40,  50,  45, 105, 120,  95
 	;   hp  atk  def  spd  sat  sdf
-
+	;       +15  +15            +25
 	db PSYCHIC, PSYCHIC
 	db 100 ; catch rate
 	db 140 ; base exp
-	db NO_ITEM ; item 1
-	db NO_ITEM ; item 2
+	db TWISTEDSPOON ; item 1
+	db TWISTEDSPOON ; item 2
 	db 63 ; gender
 	db 100 ; unknown
-	db 20 ; step cycles to hatch
+	db 4 ; step cycles to hatch
 	db 5 ; unknown
 	dn 6, 6 ; frontpic dimensions
 	db 0, 0, 0, 0 ; padding
