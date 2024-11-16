a0925a15b4e1d8b7b35e10785af766ec3aeb3eb9
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-16 12:58:07 -0500

    Update mareep.asm

--- data/base_stats/mareep.asm
+++ data/base_stats/mareep.asm
@@ -1,16 +1,16 @@
 	db MAREEP ; 179
 
-	db  55,  40,  40,  35,  65,  45
+	db  55,  40,  50,  40,  65,  45
 	;   hp  atk  def  spd  sat  sdf
-
+	;            +10   +5       
 	db ELECTRIC, ELECTRIC
 	db 235 ; catch rate
 	db 56 ; base exp
-	db NO_ITEM ; item 1
-	db NO_ITEM ; item 2
+	db MYSTERYBERRY ; item 1
+	db MYSTERYBERRY ; item 2
 	db 127 ; gender
 	db 100 ; unknown
-	db 20 ; step cycles to hatch
+	db 4 ; step cycles to hatch
 	db 5 ; unknown
 	dn 5, 5 ; frontpic dimensions
 	db 0, 0, 0, 0 ; padding
