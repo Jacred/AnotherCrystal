c5a5b9843661143db4496013db9c57e31a43e6f2
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-16 13:01:13 -0500

    Update spinarak.asm

--- data/base_stats/spinarak.asm
+++ data/base_stats/spinarak.asm
@@ -1,16 +1,16 @@
 	db SPINARAK ; 167
 
-	db  40,  60,  40,  30,  40,  40
+	db  50,  75,  50,  30,  50,  50
 	;   hp  atk  def  spd  sat  sdf
-
+	;  +10  +15  +10       +10  +10
 	db BUG, POISON
 	db 255 ; catch rate
 	db 50 ; base exp
 	db NO_ITEM ; item 1
-	db NO_ITEM ; item 2
+	db BERRY ; item 2
 	db 127 ; gender
 	db 100 ; unknown
-	db 15 ; step cycles to hatch
+	db 3 ; step cycles to hatch
 	db 5 ; unknown
 	dn 5, 5 ; frontpic dimensions
 	db 0, 0, 0, 0 ; padding
