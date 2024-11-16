2d3361f1c7c815553098e865127fa6d52f858637
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-16 14:22:27 -0500

    Update zubat.asm

--- data/base_stats/zubat.asm
+++ data/base_stats/zubat.asm
@@ -1,16 +1,16 @@
 	db ZUBAT ; 041
 
-	db 40, 45, 35, 55, 30, 40
-	;  hp  atk  def  spd  sat  sdf
-
+	db  50,  55,  45,  55,  35,  45
+	;   hp  atk  def  spd  sat  sdf
+	;  +10  +10  +10        +5   +5
 	db POISON, FLYING
 	db 255 ; catch rate
 	db 49 ; base exp
-	db NO_ITEM ; item 1
-	db NO_ITEM ; item 2
+	db BITTER_BERRY ; item 1
+	db BITTER_BERRY ; item 2
 	db 127 ; gender
 	db 100 ; unknown
-	db 15 ; step cycles to hatch
+	db 3 ; step cycles to hatch
 	db 5 ; unknown
 	dn 5, 5 ; frontpic dimensions
 	db 0, 0, 0, 0 ; padding
